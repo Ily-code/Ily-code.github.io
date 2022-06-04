6f9d4cde5261a396bf0a4681b244a5976103c0c2
ily-code <rexz5042@gmail>
2022-06-04 21:11:21 +0700

    update 4-6-2022

---
+++ 3.php
@@ -0,0 +1,21 @@
+<?php
+
+$nama="noname";
+$nim="12345";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Halo Selamat datang <?php echo $nama; ?></h1>
+<h1>Nim anda <?php echo $nim;?> </h1>
+</body>
+</html>
